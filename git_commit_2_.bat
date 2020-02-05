988f146aa5a2286b228285750a01fab924cd23b4
Zanxi <consultantweb25@gmail.com>
2020-02-05 19:43:45 +0600

    Spektrometr<Read data from com port> c++ mfc

---
+++ git_commit_2_.bat
@@ -0,0 +1,7 @@
+cls
+git init
+git add *.*
+git commit -m "Spektrometr<Read data from com port> c++ mfc"
+git remote add origin https://github.com/zanxi/Float3bytenormalize.git
+git push -u origin master
+
